f78c4686297dc35ad2425e6c5fedb63a61baddd7
shxchen2007 <160296424+shxchen2007@users.noreply.github.com>
2024-06-20 10:36:52 +0800

    ghfdg

--- 8/data.js
+++ 8/data.js
@@ -219,7 +219,7 @@ var APP_DATA = {
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
-    "fullscreenButton": false,
-    "viewControlButtons": false
+    "fullscreenButton": true,
+    "viewControlButtons": true
   }
 };
